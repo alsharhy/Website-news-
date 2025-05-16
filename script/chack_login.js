f0101078053467aad3b6d232f6f741009a24927a
alsharhy <dhayfallahalsharhi@gmail.com>
2025-05-16 14:56:38 +0300

    أول رفع للملفات

---
+++ script/chack_login.js
@@ -0,0 +1,18 @@
+ document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();  // منع إعادة تحميل الصفحة
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === 'admin' && password === 'admin') {
+      localStorage.setItem('userRole', 'admin');
+      window.location.href = '../pages/admin.html'; // توجيه إلى لوحة التحكم
+    } else if (username === 'user' && password === 'user') {
+      localStorage.setItem('userRole', 'user');
+      window.location.href = '../index.html'; // توجيه إلى الصفحة الرئيسية
+    } else {
+      alert('اسم المستخدم أو كلمة المرور غير صحيحة');
+    }
+  });
+
+ 
